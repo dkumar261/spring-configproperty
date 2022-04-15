0de4e47b528b9fbf64612ad358cd6be70f553360
Dinesh Kumar <Dinesh.7.Kumar@coforge.com>
2022-04-15 18:40:50 +0530

    First commit.

---
+++ src/test/java/com/practice/configproperty/ConfigpropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.configproperty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigpropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
